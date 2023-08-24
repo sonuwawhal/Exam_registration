342993b56a575e2c88b962a9f5a9a8b5c9520406
Soniya <graphicsoniya1@gmail>
2023-08-24 23:39:50 +0530

    new app1

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ function register(){
     email:email,
     };
 
+function notify(){                                                  //function for already existing user
+    let alert=document.getElementById("alert-danger")
+    alert.classList.remove("alert");
+    alert.classList.add("alert-danger");
+
+    setTimeout(()=>{
+      alert.classList.remove("alert-danger");
+      alert.classList.add("alert")
+    },2000)
+
+
+
+}
+
+
+function notifyuseradded(){                                                            // function for user added succesfully
+    let alert1=document.getElementById("alert-success")
+    alert1.classList.remove("alert")
+    alert1.classList.add("alert-success")
+    setTimeout(()=>{
+alert1.classList.remove("alert-success")
+alert1.classList.add("alert")
+    },2000)
+}
 
   let existingUser=users.filter((user)=>{               //checking for existing user
         return user.email==email
@@ -17,10 +41,11 @@ function register(){
     console.log("user exists??",existingUser);
     if(existingUser==0){
         users.push(user);
+        notifyuseradded();
         console.log(cities[Math.floor((Math.random(cities)*cities.length))]); //selecting random city
     }
     else{
-       console.log("User already exist") 
+       notify();
     }
 
     console.log(users);
